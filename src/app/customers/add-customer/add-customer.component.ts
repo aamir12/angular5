18e3300f7692f3879bf84e239e437db1e408256c
aamir12 <aamirkhan8878@gmail.com>
2021-07-28 10:09:43 +0530

    add radio button and checkbox in template driven form

--- src/app/customers/add-customer/add-customer.component.ts
+++ src/app/customers/add-customer/add-customer.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-add-customer',
-  templateUrl: './add-customer.component.html',
-  styleUrls: ['./add-customer.component.css']
+  selector: "app-add-customer",
+  templateUrl: "./add-customer.component.html",
+  styleUrls: ["./add-customer.component.css"],
 })
 export class AddCustomerComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
+  @ViewChild("empForm") empForm: NgForm;
+  emp = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    gender: "",
+    countries: [1, 2, 5],
+  };
+  countries = [
+    { id: 1, name: "Italia", checked: false },
+    { id: 2, name: "Brasile", checked: false },
+    { id: 3, name: "Florida", checked: false },
+    { id: 4, name: "Spagna", checked: false },
+    { id: 5, name: "Santo Domingo", checked: false },
+  ];
   ngOnInit() {
+    //IN case of EDIT
+    // this.countries = this.countries.map((x) => {
+    //   if (this.emp.countries.indexOf(x.id) !== -1) {
+    //     x.checked = true;
+    //   }
+    //   return x;
+    // });
   }
 
+  onchangeCountry(e: any) {
+    if (e.target.checked) {
+      this.emp.countries.push(e.target.value);
+    } else {
+      const index = this.emp.countries.findIndex((x) => x === e.target.value);
+      if (index !== -1) {
+        this.emp.countries.splice(index, 1);
+      }
+    }
+  }
+
+  RegisterEmp() {
+    console.log(this.emp.countries);
+  }
 }
--- src/app/shared.module.ts
+++ src/app/shared.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import {
-  EmailValidator,
   FormsModule,
   ReactiveFormsModule,
 } from "@angular/forms";
 
 @NgModule({
   imports: [FormsModule, ReactiveFormsModule, CommonModule],
-  declarations: [EmailValidator],
-  exports: [EmailValidator, FormsModule, ReactiveFormsModule, CommonModule],
+  declarations: [],
+  exports: [ FormsModule, ReactiveFormsModule, CommonModule],
 })
 export class SharedModule {}
