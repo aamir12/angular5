746cfe05691bfe449d914351a6f61396cea77080
pallaviagnito <pallaviagnitotechnologies@gmail.com>
2021-07-29 18:35:50 +0530

    add validations

--- src/app/customers/add-customer/add-customer.component.ts
+++ src/app/customers/add-customer/add-customer.component.ts
@@ -10,13 +10,17 @@ export class AddCustomerComponent implements OnInit {
   constructor() {}
 
   @ViewChild("empForm") empForm: NgForm;
+
   emp = {
-    firstName: "",
-    lastName: "",
-    email: "",
-    gender: "",
-    countries: [1, 2, 5],
+    firstName: null,
+    lastName: null,
+    email: null,
+    gender: null,
+    countries: [1,2],
+    state:null
   };
+
+  countryError = false;
   countries = [
     { id: 1, name: "Italia", checked: false },
     { id: 2, name: "Brasile", checked: false },
@@ -24,21 +28,29 @@ export class AddCustomerComponent implements OnInit {
     { id: 4, name: "Spagna", checked: false },
     { id: 5, name: "Santo Domingo", checked: false },
   ];
+  
+  states = [
+    {id:1,name:'MP'},
+    {id:2,name:'UP'},
+    {id:3,name:'AP'},
+    {id:4,name:'JK'}
+  ];
+
   ngOnInit() {
     //IN case of EDIT
-    // this.countries = this.countries.map((x) => {
-    //   if (this.emp.countries.indexOf(x.id) !== -1) {
-    //     x.checked = true;
-    //   }
-    //   return x;
-    // });
+    this.countries = this.countries.map((x) => {
+      if (this.emp.countries.indexOf(x.id) !== -1) {
+        x.checked = true;
+      }
+      return x;
+    });
   }
 
   onchangeCountry(e: any) {
     if (e.target.checked) {
-      this.emp.countries.push(e.target.value);
+      this.emp.countries.push(+e.target.value);
     } else {
-      const index = this.emp.countries.findIndex((x) => x === e.target.value);
+      const index = this.emp.countries.findIndex((x) => x === +e.target.value);
       if (index !== -1) {
         this.emp.countries.splice(index, 1);
       }
@@ -46,6 +58,22 @@ export class AddCustomerComponent implements OnInit {
   }
 
   RegisterEmp() {
-    console.log(this.emp.countries);
+    this.markAllTouched(this.empForm);
+    if(this.empForm.valid){
+      console.log(this.emp);
+    }
+  }
+  
+  
+  markAllTouched(form:NgForm){
+    for (const key in form.controls) {
+      if (form.controls.hasOwnProperty(key) && form.controls[key].hasOwnProperty('touched')) {
+         (form.controls[key] as any).touched  = true;
+      }
+    }
+    this.countryError = false
+    if(this.emp.countries.length==0){
+      this.countryError = true;
+    }
   }
 }
