5939480933652a428c2dd88ddb928e48e7bac5c0
pallaviagnito <pallaviagnitotechnologies@gmail.com>
2021-08-18 19:25:48 +0530

    add pdf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,22 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { CurrencyPipe, DatePipe } from "@angular/common";
 
+import { PdfViewerModule } from "ng2-pdf-viewer";
+import { HttpClientModule } from "@angular/common/http";
+import { Daterangepicker } from "ng2-daterangepicker";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+
 import { AppRoutingModule } from "./app-routing.module";
 import { SharedModule } from "./shared.module";
 import { CrudService } from "./crud-template/crud.service";
-import { HttpClientModule } from "@angular/common/http";
 import { MyDatePickerModule } from "../../node_modules/angular4-datepicker/src/my-date-picker";
 
 import { AppComponent } from "./app.component";
-import { Daterangepicker } from "ng2-daterangepicker";
 import { PdfComponent } from "./pdf/pdf.component";
 import { TemplatingComponent } from "./templating/templating.component";
 import { DatepickerComponent } from "./datepicker/datepicker.component";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { GitcommandComponent } from './gitcommand/gitcommand.component';
+import { GitcommandComponent } from "./gitcommand/gitcommand.component";
 
 @NgModule({
   declarations: [
@@ -32,9 +35,11 @@ import { GitcommandComponent } from './gitcommand/gitcommand.component';
     Daterangepicker,
     ReactiveFormsModule,
     FormsModule,
+    PdfViewerModule,
     AppRoutingModule,
   ],
   providers: [CrudService, DatePipe, CurrencyPipe],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
+platformBrowserDynamic().bootstrapModule(AppModule);
--- src/app/gitcommand/gitcommand.component.ts
+++ src/app/gitcommand/gitcommand.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-gitcommand',
-  templateUrl: './gitcommand.component.html',
-  styleUrls: ['./gitcommand.component.css']
+  selector: "app-gitcommand",
+  templateUrl: "./gitcommand.component.html",
+  styleUrls: ["./gitcommand.component.css"],
 })
 export class GitcommandComponent implements OnInit {
-
-  constructor() { }
-
+  constructor(private http: HttpClient) {}
+  pdfUrl: any;
   ngOnInit() {
-  }
+    let headers = new HttpHeaders();
+    headers = headers.set("Accept", "application/pdf");
 
+    this.http
+      .get("http://localhost:3000/api/pdf/create", {
+        headers: headers,
+        responseType: "arraybuffer",
+      })
+      .subscribe((response) => {
+        let file = new Blob([response], { type: "application/pdf" });
+        const url = URL.createObjectURL(file);
+        console.log(url);
+        this.pdfUrl = url;
+      });
+  }
 }
